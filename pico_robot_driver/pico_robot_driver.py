ca7e4560c7cfc0070f1d3e3d901249e463bf18c0
Jacek Grzybowski <totospl99@gmail.com>
2023-11-12 20:06:44 +0100

    init

---
+++ pico_robot_driver/pico_robot_driver.py
@@ -0,0 +1,67 @@
+import time
+import serial
+import json
+import rclpy
+import threading
+
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class DriverNode(Node):
+    def __init__(self):
+        super().__init__('pico_robot_driver')
+        self.publisher = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+        self.loop_run = True
+
+        self.ser = None
+        try:
+            self.ser = serial.Serial("/dev/ttyACM0")
+        except Exception as error:
+            self.get_logger().error(error)
+            return
+        self.t = threading.Thread(target=self.__logic_loop)
+        self.t.start()
+
+
+    def stop_loop(self):
+        self.loop_run = False
+        self.t.join()
+
+    def __logic_loop(self):
+        while self.loop_run:
+            self.ser.reset_input_buffer()
+            t = self.ser.readline()[:-2]
+            try:
+                out = json.loads(t)
+                print(out)
+            except:
+                pass
+            time.sleep(0.1)
+
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f"Hello World: {self.i}"
+        self.publisher.publish(msg)
+        self.get_logger().info(f"Publishing: {msg.data}")
+        self.i += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    driver_node = DriverNode()
+    if driver_node.ser is not None:
+        rclpy.spin(driver_node)
+        ser.close()
+
+    driver_node.stop_loop()
+    driver_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+

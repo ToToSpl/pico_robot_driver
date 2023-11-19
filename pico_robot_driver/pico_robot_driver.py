ff8c9effb6120ba2f43064eab056c6b7d5d2a290
Jacek Grzybowski <totospl99@gmail.com>
2023-11-19 21:37:27 +0100

    added param for serial_port

--- pico_robot_driver/pico_robot_driver.py
+++ pico_robot_driver/pico_robot_driver.py
@@ -8,9 +8,11 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+
 class DriverNode(Node):
     def __init__(self):
         super().__init__('pico_robot_driver')
+        self.declare_parameter('serial_port', '/dev/ttyACM0')
         self.publisher = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -18,14 +20,15 @@ class DriverNode(Node):
         self.loop_run = True
 
         self.ser = None
+        serial_port = self.get_parameter(
+            'serial_port').get_parameter_value().string_value
         try:
-            self.ser = serial.Serial("/dev/ttyACM0")
+            self.ser = serial.Serial(serial_port)
         except Exception as error:
             self.get_logger().error(error)
             return
         self.t = threading.Thread(target=self.__logic_loop)
         self.t.start()
-
 
     def stop_loop(self):
         self.loop_run = False
@@ -33,6 +36,8 @@ class DriverNode(Node):
 
     def __logic_loop(self):
         while self.loop_run:
+            serial_port = self.get_parameter(
+                'serial_port').get_parameter_value().string_value
             self.ser.reset_input_buffer()
             t = self.ser.readline()[:-2]
             try:
@@ -41,7 +46,7 @@ class DriverNode(Node):
             except:
                 pass
             time.sleep(0.1)
-
+        self.ser.close()
 
     def timer_callback(self):
         msg = String()
@@ -50,18 +55,19 @@ class DriverNode(Node):
         self.get_logger().info(f"Publishing: {msg.data}")
         self.i += 1
 
+
 def main(args=None):
     rclpy.init(args=args)
 
     driver_node = DriverNode()
     if driver_node.ser is not None:
         rclpy.spin(driver_node)
-        ser.close()
+        driver_node.ser.close()
 
     driver_node.stop_loop()
     driver_node.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
     main()
-
